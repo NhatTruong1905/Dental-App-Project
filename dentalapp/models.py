a5983eaac01817b99d391a5c5ccf144568523d18
Nguyễn Tuấn <tuan-nv0505@MacBook-Pro-cua-Nguyen.local>
2025-12-12 17:49:22 +0700

    feat: update models.py

--- dentalapp/models.py
+++ dentalapp/models.py
@@ -1,37 +1,36 @@
-from dentalapp import db
-from sqlalchemy import Integer, String, DATE, DateTime, Double, Boolean, ForeignKey, Enum, UniqueConstraint
+from dentalapp import db, app
+from sqlalchemy import Column, Integer, String, DATE, DateTime, Double, Boolean, ForeignKey, Enum, UniqueConstraint, Column
 from sqlalchemy.orm import relationship
-import enum as VAT
+from enum import Enum as VAT
 
 
 class BaseModel(db.Model):
     __abstract__ = True
 
-    id = db.Column(Integer, primary_key=True, autoincrement=True)
-    active = db.Column(Boolean, default=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    active = Column(Boolean, default=True)
 
 class Person(BaseModel):
     __abstract__ = True
 
-    name = db.Column(String(255), nullable=False)
-    birthday = db.Column(DATE, nullable=False)
-    phone = db.Column(String(20), nullable=False)
-    address = db.column(String(255), nullable=False)
+    name = Column(String(255), nullable=False)
+    birthday = Column(DATE, nullable=False)
+    phone = Column(String(20), nullable=False)
+    address = Column(String(255), nullable=False)
 
     def __str__(self):
         return self.name
 
 service_paitents = db.Table(
     'service_paitents',
-    db.Column('id', Integer, primary_key=True, autoincrement=True),
-    db.Column('service_id', Integer, ForeignKey('service.id'), nullable=False),
-    db.Column('paitents_id', Integer, ForeignKey('paitents.id'), nullable=False),
+    Column('service_id', Integer, ForeignKey('service.id'), primary_key=True),
+    Column('paitents_id', Integer, ForeignKey('paitents.id'), primary_key=True),
 )
 
 class Service(BaseModel):
     __tablename__ = "service"
-    name = db.Column(String(100), nullable=False)
-    price = db.Column(Double, nullable=False)
+    name = Column(String(100), nullable=False)
+    price = Column(Double, nullable=False)
 
     def __str__(self):
         return self.name
@@ -39,7 +38,9 @@ class Service(BaseModel):
 class Paitents(Person):
     __tablename__ = "paitents"
     services = relationship("Service", secondary="service_paitents", backref="paitents", lazy=True)
-    doctor = relationship("Doctor", secondary="appointment_schedule", backref="paitents", lazy=True)
+    medicines = relationship("Medicine", secondary="medicine_paitents", backref="paitents", lazy=True)
+    bill = relationship("Bill", backref="paitents", lazy=True)
+
 
 class Doctor(Person):
     __tablename__ = "doctor"
@@ -47,35 +48,59 @@ class Doctor(Person):
 
 class AppointmentSchedule(BaseModel):
     __tablename__ = "appointment_schedule"
-    date_time = db.Column(DateTime, nullable=False)
-    id_doctor = db.Column(Integer, ForeignKey('doctor.id'), nullable=False)
-    id_paitents = db.Column(Integer, ForeignKey('paitents.id'), nullable=False)
-    description = db.Column(String(255), nullable=False)
+    date_time = Column(DateTime, nullable=False)
+    id_doctor = Column(Integer, ForeignKey('doctor.id'), nullable=False)
+    id_paitents = Column(Integer, ForeignKey('paitents.id'), nullable=False)
+    description = Column(String(255), nullable=False)
+
+    paitents = relationship("Paitents", backref="apppointment", lazy=True)
+    doctor = relationship("Doctor", backref="schedules", lazy=True)
 
     __table_args__ = (
-        UniqueConstraint('id_doctor', 'date_time')
+        UniqueConstraint('id_doctor', 'date_time'),
     )
 
+medicine_paitents = db.Table(
+    'medicine_paitents',
+    Column('medicine_id', Integer, ForeignKey('medicine.id'), primary_key=True),
+    Column('paitents_id', Integer, ForeignKey('paitents.id'), primary_key=True),
+)
+
 class Medicine(BaseModel):
-    name = db.Column(String(100), nullable=False)
-    price = db.Column(Double, nullable=False)
-    production_date = db.Column(DATE, nullable=False)
-    expiration_date = db.Column(DATE, nullable=False)
+    __tablename__ = "medicine"
+    name = Column(String(100), nullable=False)
+    price = Column(Double, nullable=False)
+    production_date = Column(DATE, nullable=False)
+    expiration_date = Column(DATE, nullable=False)
 
     def __str__(self):
         return self.name
 
 class VATEnum(VAT):
-    VAT10PER = 10
-    VAT2PER = 2
+    VAT_10_PER = 10
+    VAT_2_PER = 2
 
 class Bill(BaseModel):
-    total_medicine = db.Column(Double, nullable=False)
-    total_service = db.Column(Double, nullable=False)
-    vat = db.Column(Enum(VATEnum), default=VATEnum.VAT10PER)
+    total_medicine = Column(Double, nullable=False)
+    total_service =  Column(Double, nullable=False)
+    total = Column(Double, nullable=False)
+    name = Column(String(255), nullable=False)
+    vat = Column(Enum(VATEnum), default=VATEnum.VAT_10_PER)
+    paitents_id = Column(Integer, ForeignKey('paitents.id'), nullable=False)
 
 class TreatmentCard(BaseModel):
-    pass
+    id_doctor = Column(Integer, ForeignKey('doctor.id'), nullable=False)
+    id_paitents = Column(Integer, ForeignKey('paitents.id'), nullable=False)
+    description = Column(String(255), nullable=False)
+
+    paitents = relationship("Paitents", backref="treatment", lazy=True)
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        db.session.commit()
+
 
 
 
