f643a7c4bfbd178bf94cd62946c18704263ed4b3
icondenimStore <tn696199@gmail.com>
2025-12-15 16:22:36 +0700

    fix: Fix name functions of index.py

--- dentalapp/dao/appointment_schedules.py
+++ dentalapp/dao/appointment_schedules.py
@@ -2,13 +2,13 @@ from dentalapp.models import AppointmentSchedule
 from sqlalchemy import func
 
 
-def count_appointment_schedules(doctor_id=None, check_date=None):
+def count_appointment_schedules(doctor_id, check_date):
     return AppointmentSchedule.query.filter(
         (AppointmentSchedule.doctor_id == doctor_id) & (
                 func.date(AppointmentSchedule.datetime) == check_date)).count() <= 5
 
 
-def load_appointment_schedules(doctor_id=None, check_date=None):
+def load_appointment_schedules(doctor_id, check_date):
     appointment_of_doctors = count_appointment_schedules(doctor_id=doctor_id, check_date=check_date)
     query = AppointmentSchedule.query
 
