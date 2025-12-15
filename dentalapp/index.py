7381b4c139359ffca4c6311734d34ee25c9a48b9
Nguyễn Tuấn <tuan-nv0505@MacBook-Pro-cua-Nguyen.local>
2025-12-15 14:12:41 +0700

    fix: directory organization

--- dentalapp/index.py
+++ dentalapp/index.py
@@ -1,17 +1,14 @@
+from dentalapp.routes import home
 from dentalapp import app, login
-from flask import render_template
-from dentalapp.admin import *
-from dentalapp.dao import get_current_user
-
-
-@app.route('/')
-def home():
-    return render_template('index.html')
+from dao import user
 
 @login.user_loader
 def load_user(user_id):
-    return get_current_user(user_id)
-
+    return user.get_current_user(user_id)
 
 if __name__ == '__main__':
+    app.register_blueprint(home.home_bp)
+
+
     app.run(debug=True)
+
