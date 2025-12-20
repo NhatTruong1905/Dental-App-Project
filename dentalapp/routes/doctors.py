5b7e524a012e688b4fe2347169370c508a39a0be
icondenimStore <tn696199@gmail.com>
2025-12-20 15:14:07 +0700

    feat: Update api & detail.html

--- dentalapp/routes/doctors.py
+++ dentalapp/routes/doctors.py
@@ -9,3 +9,13 @@ doctors_bp = Blueprint('doctors', __name__)
 def render_doctors():
     data_doctors = doctors.load_doctors()
     return render_template('doctors.html', doctors=data_doctors)
+
+
+@doctors_bp.route('/doctors/<int:doctor_id>')
+def render_detail_doctor(doctor_id):
+    doctor = doctors.get_infor_doctor(doctor_id)
+
+    if not doctor:
+        return "Không tìm thấy bác sĩ", 404
+
+    return render_template('detail.html', doctor=doctor)
