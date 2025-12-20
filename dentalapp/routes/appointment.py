33362c0c5671318fad004e529cb52a81bbb0d679
Nguyễn Tuấn <tuan-nv0505@MacBook-Pro-cua-Nguyen.local>
2025-12-21 03:39:42 +0700

    feat: create appointment UI

---
+++ dentalapp/routes/appointment.py
@@ -0,0 +1,8 @@
+from flask import render_template, Blueprint
+
+appointment_bp = Blueprint('appointment', __name__)
+
+
+@appointment_bp.route('/appointment')
+def render_appointment():
+    return render_template('appointment.html')
