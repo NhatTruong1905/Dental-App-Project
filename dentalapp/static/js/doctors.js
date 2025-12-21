09d245c3e5e4c40d1caaa200d6a989f2b1189c71
Nguyễn Tuấn <tuan-nv0505@MacBook-Pro-cua-Nguyen.local>
2025-12-21 18:32:34 +0700

    fix: bug select date

--- dentalapp/static/js/doctors.js
+++ dentalapp/static/js/doctors.js
@@ -4,18 +4,18 @@ function loadDoctor(date) {
     fetch(`/api/doctors/check-date?date=${date}`, {
     }).then(res => res.json()).then(data => {
         let doctorSelect = document.getElementById("doctor-select")
-        doctorSelect.innerHTML = ''
 
-        let option = document.createElement('option')
         if (data.length > 0) {
-            data.forEach(d => {
-                option.value = d.id
-                option.text = `${d.name}`
-                doctorSelect.appendChild(option)
-            })
+            for (doctor of data) {
+                let option = document.createElement('option')
+                option.textContent = doctor["name"];
+                option.id = doctor["id"];
+                doctorSelect.appendChild(option);
+            }
         } else {
-            option.text = `Ngày ${date} không có bác sĩ trống lịch hoặc rảnh`
-            doctorSelect.appendChild(option)
+            console.log("ok")
+            let selected = document.getElementById("selected");
+            selected.textContent = `Ngày ${date} không có bác sĩ hoặc bận!`;
         }
     })
 }
