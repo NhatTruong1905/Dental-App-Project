ca2d11148b55aa208740c98722faf3d65d0cbed2
icondenimStore <tn696199@gmail.com>
2025-12-25 15:15:43 +0700

    fix Page final

--- dentalapp/__init__.py
+++ dentalapp/__init__.py
@@ -8,8 +8,8 @@ import cloudinary
 app = Flask(__name__)
 
 app.secret_key = "!@#$%jasbej%$^(+eiwqbacjfas12399HBAS59^##GSDFG%%jjs;zs4$$"
-app.config["SQLALCHEMY_DATABASE_URI"] ="mysql+pymysql://root:tuannv0505@localhost/dentaldb?charset=utf8mb4"
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root%40123@localhost/dentaldb?charset=utf8mb4'
+# app.config["SQLALCHEMY_DATABASE_URI"] ="mysql+pymysql://root:tuannv0505@localhost/dentaldb?charset=utf8mb4"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root%40123@localhost/dentaldb?charset=utf8mb4'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.config['PAGE_SIZE'] = 6
 
